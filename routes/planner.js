95a1df1ced693e0899da477d9be78fc839c44699
Stephanie C <chang.m.steph@gmail.com>
2022-10-08 12:25:09 -1000

    Changed Planner to table

--- routes/planner.js
+++ routes/planner.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+let plans = {
+    7: "Wake up",
+    8: "Study",
+    18: "Eat Dinner",
+}
+
 router.get('/', (req, res) => {
-    res.render('planner');
+    res.render('planner', {plans: plans});
 });
 
 router.post('/', (req, res) => {
