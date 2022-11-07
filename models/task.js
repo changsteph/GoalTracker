d12d1f8b63feda97ae2a36f53d83fba2fc6a012c
Stephanie C <chang.m.steph@gmail.com>
2022-11-07 11:14:54 -1000

    Ability to add tasks and subtasks

--- models/task.js
+++ models/task.js
@@ -1,26 +1,18 @@
 const mongoose = require('mongoose');
 const Project = require('./project');
 
-const subtaskSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    description: String
-});
-
 const taskSchema = mongoose.Schema({
-    project: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Project'
-    },
-    title: {
+    // project: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Project'
+    // },
+    name: {
         type: String,
         required: true
     },
     description: String,
     deadline: String,
-    subtasks: [subtaskSchema]
+    subtasks: [String]
 });
 
 module.exports = mongoose.model('Task', taskSchema);
--- routes/projects.js
+++ routes/projects.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Project = require('../models/project');
+const Task = require('../models/task');
 
 router.get('/', async (req, res) => {
     try{
@@ -12,24 +13,64 @@ router.get('/', async (req, res) => {
     }
 })
 
-router.get('/new', (req, res) => {
-    res.render('projects/new');
-})
+router.route('/new')
+    .get((req, res) => {
+        res.render('projects/new');
+    })
+    .post(async (req, res) => {
+        try{
+            const project = new Project(req.body.project);
+            project.tasks = [];
+            project.notes = [];
+            await project.save();
+            res.redirect(`/projects/${project.id}`)
+        } catch{
+            res.send('Opps :(');
+        }
+    });
 
-router.post('/new', async (req, res) => {
-    // console.log(req.body);
-    // res.send('New Project')
-    try{
-        const project = new Project(req.body.project);
-        await project.save();
-        res.redirect(`/projects/${project.id}`)
-    } catch{
-        res.send('Opps :(');
-    }
-})
+router.route('/:id')
+    .get(async (req, res) => {
+        try{
+            const project = await Project.findById(req.params.id).populate('tasks');
+            res.render('projects/show', { project });
+        } catch{
+            res.send(req.params.id);
+        }
+    })
+    .post(async (req, res) => {
+        try{
+            const task = new Task(req.body.task);
+            task.subtasks = [];
+            const project = await Project.findById(req.params.id);
+            project.tasks.push(task);
+            await task.save();
+            await project.save();
+            res.redirect(`/projects/${project._id}/tasks/${task._id}`)
+        } catch{
+            res.redirect(`/projects/${req.params.id}`);
+        }
+    });
 
-router.get('/:id', async (req, res) => {
-    res.send(req.params.id);
-});
+router.route('/:id/tasks/:taskId')
+    .get(async (req, res) => {
+        try{
+            const {id} = req.params;
+            const task = await Task.findById(req.params.taskId);
+            res.render('projects/task', { task , id});
+        }catch{
+            res.redirect(`/projects/${req.params.id}`);
+        }
+    })
+    .post(async (req, res) => {
+        try{
+            const task = await Task.findById(req.params.taskId);
+            task.subtasks.push(req.body.subtask);
+            await task.save();
+            res.redirect(`/projects/${req.params.id}/tasks/${task._id}`);
+        }catch{
+            res.redirect(`/projects/${req.params.id}`);
+        }
+    })
 
 module.exports = router;
